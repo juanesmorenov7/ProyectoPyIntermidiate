35577e3876affe4d20e9ecbeb71a1643d1931fb9
Juan Esteban Moreno Vergara <esteban.moreno@rappi.com>
2023-10-26 11:41:49 -0500

    actualización del código donde se agrega un input request para el país y se agrega una gráfica de pie chart, además en la función principal se agregó el manejo de errores

--- BarsStatic/functions.py
+++ BarsStatic/functions.py
@@ -1,9 +1,17 @@
 import matplotlib.pyplot as plt
 
 def filtro(dict, country):
-  res = list(filter(lambda x: x['Country/Territory'] if x['Country/Territory'] == country.capitalize() else None, dict))[0] #--> con esto escojo la primera llave dentro de la lista(solo hay una)
-  population_data = {int(''.join(filter(str.isdigit, key))): int(value) for key, value in res.items() if key.endswith('Population')}
-  return population_data
+  paises = [item['Country/Territory'] for item in dict]
+  try:
+    if country.capitalize() not in paises:
+        raise IndexError(f'Escoge un país de la lista de arriba')
+    res = list(filter(lambda x: x['Country/Territory'] if x['Country/Territory'] == country.capitalize() else None, dict))[0] #--> con esto escojo la primera llave dentro de la lista(solo hay una) y uso filter para traerme solo la que cumple la condición.
+    population_data = {int(''.join(filter(str.isdigit, key))): int(value) for key, value in res.items() if key.endswith('Population')}
+    labels = [x for x in population_data.keys()]
+    values = [x for x in population_data.values()]
+    return labels, values
+  except IndexError as index:
+    print(index)
 
 def generate_bar_chart(labels, values):
   label = labels
@@ -15,3 +23,9 @@ def generate_bar_chart(labels, values):
 def list_count(dict):
   paises = [item['Country/Territory'] for item in dict]
   return paises
+
+def generate_pie_chart(labels, values):
+  fig, ax = plt.subplots()
+  ax.pie(values, labels=labels)
+  ax.axis('equal')
+  plt.show()
--- BarsStatic/main.py
+++ BarsStatic/main.py
@@ -3,10 +3,13 @@ import read_data as r
 
 
 if __name__ == '__main__':
-    datos = r.read_csv('./data.csv')
-    prueba = f.list_count(datos)
-    final = f.filtro(datos, 'Colombia')
-    labels = list(final.keys())
-    values = list(final.values())
-    print(final)
-    f.generate_bar_chart(labels, values)
+  datos = r.read_csv('./data.csv')
+  prueba = f.list_count(datos)
+  pais = input(f'escoje un país: ')
+  print(f'País escogido es --> {pais}')
+  final = f.filtro(datos,pais)
+  print(final)
+  # labels = list(final.keys())
+  # values = list(final.values())
+  f.generate_bar_chart(final[0],final[1])
+  f.generate_pie_chart(final[0],final[1])
